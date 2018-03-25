ae84d7abbd5e0b42196103de932ebea98e65e486
zhangchao <zhangchao@aladingbank.com>
2018-03-25 15:18:54 +0800

    初始提交—zc

---
+++ git_ptoject/app/src/main/java/com/example/zhangchao/gitproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.zhangchao.gitproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(MainActivity.class.getSimpleName(),"初始提交");
+    }
+}
