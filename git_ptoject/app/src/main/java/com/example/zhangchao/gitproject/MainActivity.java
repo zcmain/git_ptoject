128b945bb2bf2e437d24b887f2f53f626364dcc8
zhangchao <zhangchao@aladingbank.com>
2018-03-25 16:37:35 +0800

    Develop

--- git_ptoject/app/src/main/java/com/example/zhangchao/gitproject/MainActivity.java
+++ git_ptoject/app/src/main/java/com/example/zhangchao/gitproject/MainActivity.java
@@ -10,22 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(MainActivity.class.getSimpleName(),"初始提交");
-        Log.d(MainActivity.class.getSimpleName(),"第二次提交");
-        Log.d(MainActivity.class.getSimpleName(),"第三次提交");
-
-
-        Log.d(MainActivity.class.getSimpleName(),"---------------新分支zcmain1被创建了");
-        Log.d(MainActivity.class.getSimpleName(),"分支【zcmain1】变更啦");
-        Log.d(MainActivity.class.getSimpleName(),"分支【zcmain1】变更啦");
-        Log.d(MainActivity.class.getSimpleName(),"---------------新分支zcmain2被创建了");
-        Log.d(MainActivity.class.getSimpleName(),"分支【zcmain2】变更啦");
-
-        Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦");
-        Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦2");
-        Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦3");
-        Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦4");
-        Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦5");
-
+        Log.d(MainActivity.class.getSimpleName(),"创建本地分支devlop");
     }
 }
