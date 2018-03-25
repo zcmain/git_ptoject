b12d256494b972b522a01b504f4dd1d6c92d37b9
zhangchao <zhangchao@aladingbank.com>
2018-03-25 15:57:30 +0800

    【master】变更啦

--- git_ptoject/app/src/main/java/com/example/zhangchao/gitproject/MainActivity.java
+++ git_ptoject/app/src/main/java/com/example/zhangchao/gitproject/MainActivity.java
@@ -19,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦2");
         Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦3");
         Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦4");
+        Log.d(MainActivity.class.getSimpleName(),"本地【master】变更啦5");
+
     }
 }
